a98c8cda2ca0826250811df8b6b3516e70e267d9
Pedro David Rodríguez Sánchez <pedrodavidlp3@gmail.com>
2016-10-29 15:54:39 +0200

    Updating files

--- bin/bot.js
+++ bin/bot.js
@@ -3,13 +3,11 @@
 
 var Jarvis = require('../lib/jarvis');
 
-var token = process.env.BOT_API_KEY;
-var dbPath = process.env.BOT_DB_PATH;
+var token = process.env.BOT_API_KEY
 var name = process.env.BOT_NAME;
 
 var jarvis = new Jarvis({
     token: token,
-    dbPath: dbPath,
     name: name
 });
 
